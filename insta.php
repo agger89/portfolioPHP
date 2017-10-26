c3b7b4260a7a2ee2d4d65b0be66e606e6ec2cd2b
agger89 <33017288+agger89@users.noreply.github.com>
2017-10-26 12:03:55 +0900

    과제 1 완료
    과제1을 완료함

---
+++ insta.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <!-- 메타 태그 -->
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Instagram</title>
+    <meta name="title" content="인스타그램">
+    <!-- css -->
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/helper.css">
+    <link rel="stylesheet" href="css/default.css">
+    <link rel="stylesheet" href="css/default_small.css" media="all and (min-width: 450px)">
+    <link rel="stylesheet" href="css/default_medium.css" media="all and (min-width: 876px)">
+</head>
+<body>
+<!--    --><?php //include("landing.php")?>
+    <?php include("logged-in.php")?>
+</body>
+</html>
---
+++ logged-in.php
@@ -0,0 +1,172 @@
+<div class="header-content-wrap">
+    <div class="header-content relative clear">
+        <div class="left-content content">
+            <a href="#" class="scale"><img src="../insta/image/photo_page/insta_icon.png" alt=""></a>
+            <span class="line scale"></span>
+            <a href="#" class="scale"><img src="../insta/image/photo_page/insta_logo.png" alt=""></a>
+        </div>
+        <div class="right-content content">
+            <ul class="icon-wrap">
+                <li class="icon"><a href="#" class="block"><img src="../insta/image/photo_page/header_icon_1.png" alt=""></a></li>
+                <li class="icon"><a href="#" class="block"><img src="../insta/image/photo_page/header_icon_2.png" alt=""></a></li>
+                <li class="icon"><a href="#" class="block"><img src="../insta/image/photo_page/header_icon_3.png" alt=""></a></li>
+            </ul>
+        </div>
+        <div class="search-wrap content">
+            <div class="static-search">
+                <span class="inline-block"><img src="../insta/image/photo_page/input_search_icon.png" alt=""></span>
+                <span class="inline-block">검색</span>
+            </div>
+        </div>
+    </div>
+</div><!-- header-content-wrap -->
+<div class="body-content-wrap">
+    <div class="object-content-wrap">
+        <div class="header-title-wrap">
+            <div class="icon-wrap header-object">
+                <img src="../insta/image/photo_page/insta_color_icon.png" alt="">
+            </div>
+            <div class="text-wrap header-object">
+                <span class="title block">instagram</span>
+                <p class="sub-title">Museum Boijimans Van Benuningen</p>
+            </div>
+        </div>
+        <div class="body-image-wrap"></div>
+        <div class="footer-comment-wrap">
+            <div class="like-wrap clear">
+                <span class="like inline-block"></span>
+                <span class="comment inline-block"></span>
+                <span class="option inline-block"></span>
+            </div>
+            <div class="comment-wrap">
+                <div class="view-count">조회 <span class="status-number inline-block">1,875,539</span>회</div>
+                <p class="comment">
+                    <span class="user-name inline-block">mildoney</span>
+                    <span class="user-comment inline-block">Follow me</span>
+                </p>
+                <p class="comment">
+                    <span class="user-name inline-block">I_photografie_life</span>
+                    <span class="user-comment inline-block">Toll</span>
+                </p>
+                <p class="comment">
+                    <span class="user-name inline-block">linda_kmak</span>
+                    <span class="user-comment inline-block">Just beautiful you are</span>
+                </p>
+                <span class="record-time block">10시간 전</span>
+            </div>
+            <div class="comment-typing-board clear">
+                <form action="" class="comment-form">
+                    <textarea class="comment-textarea inline-block" placeholder="댓글 달기..."></textarea>
+                </form>
+                <a href="#" class="modal-menu inline-block"><img src="../insta/image/photo_page/modal_icon.png" alt=""></a>
+            </div>
+        </div>
+    </div>
+    <!------------------------------------------------->
+    <div class="object-content-wrap object02">
+        <div class="header-title-wrap">
+            <div class="icon-wrap header-object">
+                <img src="../insta/image/photo_page/insta_color_icon.png" alt="">
+            </div>
+            <div class="text-wrap header-object">
+                <span class="title block">instagram</span>
+                <p class="sub-title">Museum Boijimans Van Benuningen</p>
+            </div>
+        </div>
+        <div class="body-image-wrap"></div>
+        <div class="footer-comment-wrap">
+            <div class="like-wrap clear">
+                <span class="like inline-block"></span>
+                <span class="comment inline-block"></span>
+                <span class="option inline-block"></span>
+            </div>
+            <div class="comment-wrap">
+                <div class="view-count">조회 <span class="status-number inline-block">1,875,539</span>회</div>
+                <p class="comment">
+                    <span class="user-name inline-block">mildoney</span>
+                    <span class="user-comment inline-block">Follow me</span>
+                </p>
+                <p class="comment">
+                    <span class="user-name inline-block">I_photografie_life</span>
+                    <span class="user-comment inline-block">Toll</span>
+                </p>
+                <p class="comment">
+                    <span class="user-name inline-block">linda_kmak</span>
+                    <span class="user-comment inline-block">Just beautiful you are</span>
+                </p>
+                <span class="record-time block">10시간 전</span>
+            </div>
+            <div class="comment-typing-board clear">
+                <form action="" class="comment-form">
+                    <textarea class="comment-textarea inline-block" placeholder="댓글 달기...">
+                    </textarea>
+                </form>
+                <a href="#" class="modal-menu inline-block"><img src="../insta/image/photo_page/modal_icon.png" alt=""></a>
+            </div>
+        </div>
+    </div>
+    <!------------------------------------------------->
+    <div class="object-content-wrap object03">
+        <div class="header-title-wrap">
+            <div class="icon-wrap header-object">
+                <img src="../insta/image/photo_page/insta_color_icon.png" alt="">
+            </div>
+            <div class="text-wrap header-object">
+                <span class="title block">instagram</span>
+                <p class="sub-title">Museum Boijimans Van Benuningen</p>
+            </div>
+        </div>
+        <div class="body-image-wrap"></div>
+        <div class="footer-comment-wrap">
+            <div class="like-wrap clear">
+                <span class="like inline-block"></span>
+                <span class="comment inline-block"></span>
+                <span class="option inline-block"></span>
+            </div>
+            <div class="comment-wrap">
+                <div class="view-count">조회 <span class="status-number inline-block">1,875,539</span>회</div>
+                <p class="comment">
+                    <span class="user-name inline-block">mildoney</span>
+                    <span class="user-comment inline-block">Follow me</span>
+                </p>
+                <p class="comment">
+                    <span class="user-name inline-block">I_photografie_life</span>
+                    <span class="user-comment inline-block">Toll</span>
+                </p>
+                <p class="comment">
+                    <span class="user-name inline-block">linda_kmak</span>
+                    <span class="user-comment inline-block">Just beautiful you are</span>
+                </p>
+                <span class="record-time block">10시간 전</span>
+            </div>
+            <div class="comment-typing-board clear">
+                <form action="" class="comment-form">
+                    <textarea class="comment-textarea inline-block" placeholder="댓글 달기...">
+                    </textarea>
+                </form>
+                <a href="#" class="modal-menu inline-block"><img src="../insta/image/photo_page/modal_icon.png" alt=""></a>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
