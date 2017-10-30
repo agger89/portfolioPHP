e3a08085a409a2baf37213b5a0e5da3708cea87a
agger89 <33017288+agger89@users.noreply.github.com>
2017-10-30 15:36:17 +0900

    php 작업중

--- logged-in.php
+++ logged-in.php
@@ -21,6 +21,25 @@
     </div>
 </div><!-- header-content-wrap -->
 <div class="body-content-wrap">
+    <?php
+    $contents1 = [];
+    $contents1['mildoney'] = 'Follow me';
+    $contents1['I_photografie_life'] = 'Toll';
+    $contents1['linda_kmak'] = 'Just beautiful you are';
+
+    $contents2 = [];
+    $contents2['mildoney'] = 'Follow me';
+    $contents2['I_photografie_life'] = 'Toll';
+    $contents2['linda_kmak'] = 'Just beautiful you are';
+
+    $contents3 = [];
+    $contents3['mildoney'] = 'Follow me';
+    $contents3['I_photografie_life'] = 'Toll';
+    $contents3['linda_kmak'] = 'Just beautiful you are';
+
+    $contents = array($contents1, $contents2, $contents3);
+    ?>
+    <?php foreach ($contents as $key => $value) {?>
     <div class="object-content-wrap">
         <div class="header-title-wrap">
             <div class="icon-wrap header-object">
@@ -62,92 +81,93 @@
             </div>
         </div>
     </div>
+    <?php } ?>
     <!------------------------------------------------->
-    <div class="object-content-wrap object02">
-        <div class="header-title-wrap">
-            <div class="icon-wrap header-object">
-                <img src="../insta/image/photo_page/insta_color_icon.png" alt="">
-            </div>
-            <div class="text-wrap header-object">
-                <span class="title block">instagram</span>
-                <p class="sub-title">Museum Boijimans Van Benuningen</p>
-            </div>
-        </div>
-        <div class="body-image-wrap"></div>
-        <div class="footer-comment-wrap">
-            <div class="like-wrap clear">
-                <span class="like inline-block"></span>
-                <span class="comment inline-block"></span>
-                <span class="option inline-block"></span>
-            </div>
-            <div class="comment-wrap">
-                <div class="view-count">조회 <span class="status-number inline-block">1,875,539</span>회</div>
-                <p class="comment">
-                    <span class="user-name inline-block">mildoney</span>
-                    <span class="user-comment inline-block">Follow me</span>
-                </p>
-                <p class="comment">
-                    <span class="user-name inline-block">I_photografie_life</span>
-                    <span class="user-comment inline-block">Toll</span>
-                </p>
-                <p class="comment">
-                    <span class="user-name inline-block">linda_kmak</span>
-                    <span class="user-comment inline-block">Just beautiful you are</span>
-                </p>
-                <span class="record-time block">10시간 전</span>
-            </div>
-            <div class="comment-typing-board clear">
-                <form action="" class="comment-form">
-                    <textarea class="comment-textarea inline-block" placeholder="댓글 달기...">
-                    </textarea>
-                </form>
-                <a href="#" class="modal-menu inline-block"><img src="../insta/image/photo_page/modal_icon.png" alt=""></a>
-            </div>
-        </div>
-    </div>
-    <!------------------------------------------------->
-    <div class="object-content-wrap object03">
-        <div class="header-title-wrap">
-            <div class="icon-wrap header-object">
-                <img src="../insta/image/photo_page/insta_color_icon.png" alt="">
-            </div>
-            <div class="text-wrap header-object">
-                <span class="title block">instagram</span>
-                <p class="sub-title">Museum Boijimans Van Benuningen</p>
-            </div>
-        </div>
-        <div class="body-image-wrap"></div>
-        <div class="footer-comment-wrap">
-            <div class="like-wrap clear">
-                <span class="like inline-block"></span>
-                <span class="comment inline-block"></span>
-                <span class="option inline-block"></span>
-            </div>
-            <div class="comment-wrap">
-                <div class="view-count">조회 <span class="status-number inline-block">1,875,539</span>회</div>
-                <p class="comment">
-                    <span class="user-name inline-block">mildoney</span>
-                    <span class="user-comment inline-block">Follow me</span>
-                </p>
-                <p class="comment">
-                    <span class="user-name inline-block">I_photografie_life</span>
-                    <span class="user-comment inline-block">Toll</span>
-                </p>
-                <p class="comment">
-                    <span class="user-name inline-block">linda_kmak</span>
-                    <span class="user-comment inline-block">Just beautiful you are</span>
-                </p>
-                <span class="record-time block">10시간 전</span>
-            </div>
-            <div class="comment-typing-board clear">
-                <form action="" class="comment-form">
-                    <textarea class="comment-textarea inline-block" placeholder="댓글 달기...">
-                    </textarea>
-                </form>
-                <a href="#" class="modal-menu inline-block"><img src="../insta/image/photo_page/modal_icon.png" alt=""></a>
-            </div>
-        </div>
-    </div>
+<!--    <div class="object-content-wrap object02">-->
+<!--        <div class="header-title-wrap">-->
+<!--            <div class="icon-wrap header-object">-->
+<!--                <img src="../insta/image/photo_page/insta_color_icon.png" alt="">-->
+<!--            </div>-->
+<!--            <div class="text-wrap header-object">-->
+<!--                <span class="title block">instagram</span>-->
+<!--                <p class="sub-title">Los Angeles, California</p>-->
+<!--            </div>-->
+<!--        </div>-->
+<!--        <div class="body-image-wrap"></div>-->
+<!--        <div class="footer-comment-wrap">-->
+<!--            <div class="like-wrap clear">-->
+<!--                <span class="like inline-block"></span>-->
+<!--                <span class="comment inline-block"></span>-->
+<!--                <span class="option inline-block"></span>-->
+<!--            </div>-->
+<!--            <div class="comment-wrap">-->
+<!--                <div class="view-count">조회 <span class="status-number inline-block">1,875,539</span>회</div>-->
+<!--                <p class="comment">-->
+<!--                    <span class="user-name inline-block">mildoney</span>-->
+<!--                    <span class="user-comment inline-block">Follow me</span>-->
+<!--                </p>-->
+<!--                <p class="comment">-->
+<!--                    <span class="user-name inline-block">I_photografie_life</span>-->
+<!--                    <span class="user-comment inline-block">Toll</span>-->
+<!--                </p>-->
+<!--                <p class="comment">-->
+<!--                    <span class="user-name inline-block">linda_kmak</span>-->
+<!--                    <span class="user-comment inline-block">Just beautiful you are</span>-->
+<!--                </p>-->
+<!--                <span class="record-time block">10시간 전</span>-->
+<!--            </div>-->
+<!--            <div class="comment-typing-board clear">-->
+<!--                <form action="" class="comment-form">-->
+<!--                    <textarea class="comment-textarea inline-block" placeholder="댓글 달기...">-->
+<!--                    </textarea>-->
+<!--                </form>-->
+<!--                <a href="#" class="modal-menu inline-block"><img src="../insta/image/photo_page/modal_icon.png" alt=""></a>-->
+<!--            </div>-->
+<!--        </div>-->
+<!--    </div>-->
+<!--    <!------------------------------------------------->
+<!--    <div class="object-content-wrap object03">-->
+<!--        <div class="header-title-wrap">-->
+<!--            <div class="icon-wrap header-object">-->
+<!--                <img src="../insta/image/photo_page/insta_color_icon.png" alt="">-->
+<!--            </div>-->
+<!--            <div class="text-wrap header-object">-->
+<!--                <span class="title block">instagram</span>-->
+<!--                <p class="sub-title">Novosibirsk, Russia</p>-->
+<!--            </div>-->
+<!--        </div>-->
+<!--        <div class="body-image-wrap"></div>-->
+<!--        <div class="footer-comment-wrap">-->
+<!--            <div class="like-wrap clear">-->
+<!--                <span class="like inline-block"></span>-->
+<!--                <span class="comment inline-block"></span>-->
+<!--                <span class="option inline-block"></span>-->
+<!--            </div>-->
+<!--            <div class="comment-wrap">-->
+<!--                <div class="view-count">조회 <span class="status-number inline-block">1,875,539</span>회</div>-->
+<!--                <p class="comment">-->
+<!--                    <span class="user-name inline-block">mildoney</span>-->
+<!--                    <span class="user-comment inline-block">Follow me</span>-->
+<!--                </p>-->
+<!--                <p class="comment">-->
+<!--                    <span class="user-name inline-block">I_photografie_life</span>-->
+<!--                    <span class="user-comment inline-block">Toll</span>-->
+<!--                </p>-->
+<!--                <p class="comment">-->
+<!--                    <span class="user-name inline-block">linda_kmak</span>-->
+<!--                    <span class="user-comment inline-block">Just beautiful you are</span>-->
+<!--                </p>-->
+<!--                <span class="record-time block">10시간 전</span>-->
+<!--            </div>-->
+<!--            <div class="comment-typing-board clear">-->
+<!--                <form action="" class="comment-form">-->
+<!--                    <textarea class="comment-textarea inline-block" placeholder="댓글 달기...">-->
+<!--                    </textarea>-->
+<!--                </form>-->
+<!--                <a href="#" class="modal-menu inline-block"><img src="../insta/image/photo_page/modal_icon.png" alt=""></a>-->
+<!--            </div>-->
+<!--        </div>-->
+<!--    </div>-->
 </div>
 
 
