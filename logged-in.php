068f2eedd0cec4c20138341d094bb02eee2fa3ca
agger89 <33017288+agger89@users.noreply.github.com>
2017-11-06 14:16:19 +0900

    과제4 db 데이터 불러와서 값 넣기

--- logged-in.php
+++ logged-in.php
@@ -1,3 +1,10 @@
+<?php
+    $dbh = new PDO('mysql:host=localhost;dbname=contents', 'root', '111111');
+    $stmt = $dbh->prepare('SELECT * FROM member');
+    $stmt->execute();
+    $list = $stmt->fetchAll();
+
+?>
 <div class="header-content-wrap">
     <div class="header-content relative clear">
         <div class="left-content content">
@@ -21,68 +28,7 @@
     </div>
 </div><!-- header-content-wrap -->
 <div class="body-content-wrap">
-    <?php
-    $contents = [
-        [
-            "title" => "title1",
-            "image" => "../insta/image/photo_page/content01_body_img.jpg",
-            "views" => "1,111,111",
-            "comments" => [
-                    [
-                        'user-name'=>'user1-1',
-                        'comment'=>'comment1-1'
-                    ],
-                    [
-                        'user-name'=>'user1-2',
-                        'comment'=>'comment1-2'
-                    ],
-                    [
-                        'user-name'=>'user1-3',
-                        'comment'=>'comment1-3'
-                    ]
-            ],
-        ],
-        [
-            "title" => "title2",
-            "image" => "../insta/image/photo_page/content02_body_img.jpg",
-            "views" => "2,222,222",
-            "comments" => [
-                [
-                    'user-name'=>'user2-1',
-                    'comment'=>'comment2-1'
-                ],
-                [
-                    'user-name'=>'user2-2',
-                    'comment'=>'comment2-2'
-                ],
-                [
-                    'user-name'=>'user2-3',
-                    'comment'=>'comment2-3'
-                ]
-            ],
-        ],
-        [
-            "title" => "title3",
-            "image" => "../insta/image/photo_page/content03_body_img.jpg",
-            "views" => "3,333,333",
-            "comments" => [
-                [
-                    'user-name'=>'user3-1',
-                    'comment'=>'comment3-1'
-                ],
-                [
-                    'user-name'=>'user3-2',
-                    'comment'=>'comment3-2'
-                ],
-                [
-                    'user-name'=>'user3-3',
-                    'comment'=>'comment3-3'
-                ]
-            ],
-        ],
-    ];
-    ?>
-    <?php foreach ($contents as $key => $value) { ?>
+    <?php foreach ($list as $key => $value) { ?>
     <div class="object-content-wrap">
         <div class="header-title-wrap">
             <div class="icon-wrap header-object">
@@ -90,10 +36,10 @@
             </div>
             <div class="text-wrap header-object">
                 <span class="title block">instagram</span>
-                <p class="sub-title"><?php echo $value["title"] ?></p>
+                <p class="location-title"><?php echo $value['location']?></p>
             </div>
         </div>
-        <div class="body-image-wrap" style="background-image:url('<?php echo $value["image"] ?>')"></div>
+        <div class="body-image-wrap" style="background-image:url(<?php echo $value['image'] ?>)"></div>
         <div class="footer-comment-wrap test">
             <div class="like-wrap clear">
                 <span class="like inline-block"></span>
@@ -101,13 +47,11 @@
                 <span class="option inline-block"></span>
             </div>
             <div class="comment-wrap">
-                <div class="view-count">조회 <span class="status-number inline-block"><?php echo $value["views"]?></span>회</div>
-                <?php foreach ($value["comments"] as $keys => $values ){?>
+                <div class="view-count">조회 <span class="status-number inline-block"><?php echo $value['view']?></span>회</div>
                 <p class="comment">
-                    <span class="user-name inline-block"><?php echo $values['user-name'] ?></span>
-                    <span class="user-comment inline-block"><?php echo $values['comment'] ?></span>
+                    <span class="user-name inline-block"><?php echo $value['id'] ?></span>
+                    <span class="user-comment inline-block"><?php echo $value['comment'] ?></span>
                 </p>
-                <?php } ?>
                 <span class="record-time block">10시간 전</span>
             </div>
             <div class="comment-typing-board clear">
